bbc4514eab7eeef6049ba235f960a53da40fdbcb
Arash Sadeghi <arash.sadeghi@goodai.com>
2022-08-04 13:57:14 +0200

    astar added

--- mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
+++ mrim_task/mrim_planner/scripts/path_planners/grid_based/astar.py
@@ -71,8 +71,8 @@ class Node:
         a = self.pos[0] - self.goal[0]
         b = self.pos[1] - self.goal[1]
         c = self.pos[2] - self.goal[2]
-
-        raise NotImplementedError('[STUDENTS TODO] Heuristic function guiding the state space exploration not implemented. You have to finish it on your own.')
+        return np.sqrt( a**2 + b**2 + c**2 )
+        # raise NotImplementedError('[STUDENTS TODO] Heuristic function guiding the state space exploration not implemented. You have to finish it on your own.')
 # # #}
 
 # # #{ class AStar
@@ -92,13 +92,14 @@ class AStar():
         return sqrt(a**2 + b**2 + c**2)
 
     def halveAndTest(self, path):
+        # print("[+] ASTAAAAAAAAAAAR")
         pt1 = path[0]
         pt2 = path[-1]
         
         if len(path) <= 2:
             return path
 
-        raise NotImplementedError('[STUDENTS TODO] A*: path straightening is not finished. Finish it on your own.')
+        # raise NotImplementedError('[STUDENTS TODO] A*: path straightening is not finished. Finish it on your own.')
         # Tips:
         #  - divide the given path by a certain ratio and use this method recursively
 
@@ -106,9 +107,8 @@ class AStar():
         if self.grid.obstacleBetween(pt1, pt2):
 
             # [STUDENTS TODO] Replace seg1 and seg2 variables effectively
-            seg1 = path[:1]
-            seg2 = path[1:]
-
+            seg1 = self.halveAndTest(path[:len(path)//2])
+            seg2 = self.halveAndTest(path[len(path)//2:])
             seg1.extend(seg2)
             return seg1
         
